95578cfdc38eb56172a6dac84ae014decfa91b89
Andreas Krühlmann <andreas@kruhlmann.dev>
2020-07-24 00:56:01 +0200

    :hammer: Updated old make targets

--- make/dev.mk
+++ make/dev.mk
@@ -2,8 +2,6 @@ TSC ?= ./node_modules/.bin/tsc
 SAPPER ?= ./node_modules/.bin/sapper
 
 dev:
-	@tmux \
-		new-session '$(TSC) -w' \; \
-		split-window -h '$(SAPPER) dev --src $(CORE_DIR) --routes $(ROUTES_DIR)' \;
+	@$(SAPPER) dev --src $(CORE_DIR) --routes $(ROUTES_DIR)
 
 .PHONY: dev
