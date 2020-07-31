d4dec148408d222f3d2b68a0a26c6268d5157f54
Andreas Krühlmann <andreas@kruhlmann.dev>
2020-07-31 13:10:24 +0200

    :hammer: Added legacy support

--- make/build.mk
+++ make/build.mk
@@ -5,7 +5,7 @@ build: __sapper__/build
 export: __sapper__/export
 
 __sapper__/build: version.json node_modules clean
-	@$(SAPPER) build --src $(CORE_DIR) --routes $(ROUTES_DIR)
+	@$(SAPPER) build --legacy --src $(CORE_DIR) --routes $(ROUTES_DIR)
 
 __sapper__/export: version.json node_modules clean
 	@$(SAPPER) export --src $(CORE_DIR) --routes $(ROUTES_DIR)
